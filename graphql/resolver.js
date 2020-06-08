afa8996e860a2195f432057884f3b14921d8a9cf
juliuxs <juliustdp4@gmail.com>
2020-06-08 16:30:01 +0300

    Merng_Backed

---
+++ graphql/resolver.js
@@ -0,0 +1,18 @@
+const posts = require('./resolvers/posts');
+const users = require('./resolvers/users');
+const comments = require('./resolvers/comments');
+
+module.exports = {
+  Post: {
+    likeCount: (parent) => parent.likes.length,
+    commentCount: (parent) => parent.comments.length
+  },
+  Query: {
+    ...posts.Query
+  },
+  Mutation: {
+    ...users.Mutation,
+    ...posts.Mutation,
+    ...comments.Mutation
+  },
+};
