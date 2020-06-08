8d9a087c67e3cd6bb143cdb989bb34d1fbaf3b3d
juliuxs <juliustdp4@gmail.com>
2020-06-08 17:05:15 +0300

    stage_1.2

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    'mongodb+srv://juliuks:QIIv2d5rv1e0C0DG@merngappdb-hc5k4.mongodb.net/MerngAppDB?retryWrites=true&w=majority',
+  SECRET_KEY: 'some very secret key'
+};
